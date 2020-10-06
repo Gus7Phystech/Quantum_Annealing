44381f166026d801f86d92d9c217cef8c6cd0620
Gus7 <Kolya-gus2002@ya.ru>
2020-10-07 00:14:18 +0300

    Added comments

--- AND_Gate.py
+++ AND_Gate.py
@@ -2,12 +2,17 @@ import dimod
 from dimod.reference.samplers import ExactSolver
 
 #
-#constant  model:
+#constant BQM model:
 AND_BQM = dimod.BinaryQuadraticModel({'x1': 0.0, 'x2': 0.0, 'y1': 6.0},
                   {('x2', 'x1'): 2.0, ('y1', 'x1'): -4.0, ('y1', 'x2'): -4.0},
                   0, 'BINARY')
 
+
 def inp(message):
+    '''
+    :param message: message to user
+    :return: input variables
+    '''
     l = list(map(int, input(message).split()))
     try:
         return l[0], l[1]
@@ -16,17 +21,31 @@ def inp(message):
 
 
 def sample_BQM(BQM):
+    '''
+    :param BQM: BQM model
+    :return: simplified model
+    '''
     sampler = ExactSolver()
     return sampler.sample(BQM)
 
 
 def and_gate(input_x1, input_x2, sampled_not):
-    sliced_set = sampled_not.slice(0, 1)
+    '''
+    O(n) - we pass through rows in simplified model and checking for matches with our two vars.
+    When found a match - we analyse the what the answer should be.
+
+    :param input_x1: first variable in gate
+    :param input_x2: second variable in gate
+    :param sampled_not: simplified model
+    :return: answer: input_x1 AND input_x2 == True or False
+    '''
+    sliced_set = sampled_not.slice(0, 1) #first row
+
+    # x1,2 from model
     comparing_x1, comparing_x2 = sliced_set.first.sample['x1'], sliced_set.first.sample['x2']
-    i = 1
-    #while min(comparing_x1, comparing_x2) != min(input_x1, input_x2) or \
-    #        max(comparing_x1, comparing_x2) != max(input_x1, input_x2):
-    while comparing_x1 != input_x1 or comparing_x2 != input_x2:
+
+    i = 1 #num of current row
+    while comparing_x1 != input_x1 or comparing_x2 != input_x2: #work untill it is a match
         try:
             sliced_set = sampled_not.slice(i, i+1)
             comparing_x1, comparing_x2 = sliced_set.first.sample['x1'], sliced_set.first.sample['x2']
@@ -34,6 +53,8 @@ def and_gate(input_x1, input_x2, sampled_not):
             print("I can't apply AND gate to the x and y")
             break
         i += 1
+
+    #if energy is minimal and y1 from model is 1 then True
     if sliced_set.first.sample['y1'] == 1 and \
         sliced_set.data_vectors['energy'][0] == sampled_not.lowest().data_vectors['energy'][0]:
         return True
@@ -43,6 +64,7 @@ def and_gate(input_x1, input_x2, sampled_not):
 
 if __name__ == '__main__':
     sampled_model = sample_BQM(AND_BQM) #sampling model
-    #print(sampled_model)
+    #input
     in_x, in_y = inp("Please, input an x and y (with a gap between) to apply AND gate: ")
+    #answer
     print(and_gate(in_x, in_y, sampled_model))
--- NOT_Gate.py
+++ NOT_Gate.py
@@ -9,15 +9,27 @@ NOT_BQM = dimod.BinaryQuadraticModel({'x1': -1.0, 'x2': -1.0},
 
 
 def sample_BQM(BQM):
+    '''
+    :param BQM: BQM model
+    :return: simplified model
+    '''
     sampler = ExactSolver()
     return sampler.sample(BQM)
 
 
 def not_gate(input_x, sampled_not):
-    sliced_set = sampled_not.slice(0, 1)
-    comparing_x = sliced_set.first.sample['x1']
-    i = 1
-    while comparing_x != input_x:
+    '''
+    o(n) - we compare given variable to the first from model. When match we stop and give the answer
+    Answer is the second variable from the model
+
+    :param input_x: given variable
+    :param sampled_not: BQM model
+    :return: NOT input_x
+    '''
+    sliced_set = sampled_not.slice(0, 1) #first row
+    comparing_x = sliced_set.first.sample['x1'] #x1 from model
+    i = 1 #num of current row
+    while comparing_x != input_x: #work untill it is a match
         try:
             sliced_set = sampled_not.slice(i, i+1)
             comparing_x = sliced_set.first.sample['x1']
@@ -25,11 +37,14 @@ def not_gate(input_x, sampled_not):
             print("I can't apply NOT gate to the x")
             break
         i += 1
+    
+    # return second variable
     return sliced_set.first.sample['x2']
 
 
 if __name__ == '__main__':
     sampled_model = sample_BQM(NOT_BQM) #sampling model
-    #print(sampled_model)
+    #input
     in_x = int(input("Please, input an x to apply NOT gate: "))
+    #answer
     print(not_gate(in_x, sampled_model))
