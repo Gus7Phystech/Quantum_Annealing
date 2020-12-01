43c6ea50aa52859746cfb2dabe2fb3971ae1dd23
Gus7 <Kolya-gus2002@ya.ru>
2020-12-01 13:51:30 +0300

    Respects mass, space, center of gravity: fully connected qubits

--- Airbus quantum challenge/main.py
+++ Airbus quantum challenge/main.py
@@ -1,54 +1,88 @@
 import dimod
-from dimod.reference.samplers import ExactSolver
-import xlrd
+from dwave.system import EmbeddingComposite, DWaveSampler
+from dimod import BinaryQuadraticModel
+from sklearn import preprocessing
+#import xlrd
 
-book = xlrd.open_workbook('containers.xlsx')
-sheet = book.sheet_by_index(0)
+#book = xlrd.open_workbook('containers.xlsx')
+#sheet = book.sheet_by_index(0)
 
-t = []
-m = []
-for i in range(15):
-    t.append(sheet.cell(i+1, 1).value)
-    m.append(sheet.cell(i+1, 2).value)
-for i in range(15):
-    t.append(sheet.cell(i + 1, 4).value)
-    m.append(sheet.cell(i + 1, 5).value)
+t = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+m = [2134.0, 3455.0, 1866.0, 1699.0, 3500.0, 3332.0, 2578.0, 2315.0, 1888.0, 1786.0, 3277.0, 2987.0, 2534.0, 2111.0, 2607.0, 1566.0, 1765.0, 1946.0, 1732.0, 1641.0, 1800.0, 986.0, 873.0, 1764.0, 1239.0, 1487.0, 769.0, 836.0, 659.0, 765.0]
+#for i in range(15):
+#    t.append(sheet.cell(i+1, 1).value)
+#    m.append(sheet.cell(i+1, 2).value)
+#for i in range(15):
+#    t.append(sheet.cell(i + 1, 4).value)
+#    m.append(sheet.cell(i + 1, 5).value)
 
 h_a_dict = {}
 J_a_b_dict = {}
 
-L = 100
-N = 20
-n = 30
+L = 1
+N = 4
+n = 10
 
-W_p = 40 # *10**3
-W_e = 120 # *10**3
+W_p = 40*10**3
+W_e = 120*10**3
+m.append(W_p)
+m.append(W_e)
+m = preprocessing.normalize([m])[0]
+W_p, W_e, m = m[-2], m[-1], m[:-2]
 
 x_cg_e = -0.05*L
 x_cg_min = -0.1*L
 x_cg_max = 0.2*L
 x_cg_t = 0.1*L
+
 S_0_max = 22*10**3
 
-
+theta_mass = 1
+theta_space = 5
+theta_one_to_one = 5
+theta_target_x = 0.5
 for i in range(n):
     for k in range(N):
-        j = -int(N/2-0.5) + k
-        h_a_dict['Z{}_{}'.format(i, j)] = -2*W_p*m[i] + m[i]**2 - 2*N*t[i] + t[i]**2 + W_e*(x_cg_e - x_cg_max)*m[i]*(L/N*j-x_cg_min) + \
+        j = -N/2+0.5 + k
+        h_a_dict['Z{}_{}'.format(i, j)] = theta_mass*(-2*W_p*m[i] + m[i]**2)+theta_space*(- 2*N*t[i] + t[i]**2) + W_e*(x_cg_e - x_cg_max)*m[i]*(L/N*j-x_cg_min) + \
             W_e*(x_cg_e - x_cg_min)*m[i]*(L/N*j-x_cg_max) + m[i]**2*(L/N*j - x_cg_min)*(L/N*j - x_cg_max) + \
-            2*W_e*(x_cg_e - x_cg_t)*m[i]*(L/N*j-x_cg_t) + m[i]**2*(L/N*j-x_cg_t)**2 + t[i] + 1
+            theta_target_x*(2*W_e*(x_cg_e - x_cg_t)*m[i]*(L/N*j-x_cg_t) + m[i]**2*(L/N*j-x_cg_t)**2) + theta_one_to_one*(t[i] + 1)
         for i_1 in range(n):
             for k_1 in range(N):
-                j_1 = -int(N/2-0.5) + k_1
+                j_1 = -N/2+0.5 + k_1
                 J_a_b_dict['Z{}_{}'.format(i, j),
-                           'Z{}_{}'.format(i_1, j_1)] = 2*m[i]*m[i_1] + 2*t[i]*t[i_1] + 2*m[i]*m[i_1]*(L/N*j-x_cg_min)*(L/N*j_1-x_cg_max) + \
-                    2*m[i]*m[i_1]*(L/N*j-x_cg_t)*(L/N*j_1-x_cg_t)
+                           'Z{}_{}'.format(i_1, j_1)] = theta_mass*(2*m[i]*m[i_1]) + theta_space*(2*t[i]*t[i_1]) + 2*m[i]*m[i_1]*(L/N*j-x_cg_min)*(L/N*j_1-x_cg_max) + \
+                    theta_target_x*(2*m[i]*m[i_1]*(L/N*j-x_cg_t)*(L/N*j_1-x_cg_t))
 
-gamma = W_p**2 + N**2 + W_e**2*(x_cg_e - x_cg_min)*(x_cg_e - x_cg_max) + W_e**2*(x_cg_e - x_cg_t)**2 - N - n
+gamma = theta_mass*(W_p**2)+theta_space*(N**2) + W_e**2*(x_cg_e - x_cg_min)*(x_cg_e - x_cg_max) + theta_target_x*(W_e**2*(x_cg_e - x_cg_t)**2) +theta_one_to_one*(- N - n)
 
 BQM = dimod.BinaryQuadraticModel(h_a_dict, J_a_b_dict, gamma, 'BINARY')
 
-sampler = ExactSolver()
-sampled = sampler.sample(BQM)
+#sampler = ExactSolver()
+#sampled = sampler.sample(BQM)
+#print(sampled)
 
+ising_model = BQM.to_ising()
+sampler = EmbeddingComposite(DWaveSampler())
+sampled = sampler.sample_ising(h = ising_model[0], J = ising_model[1], num_reads = 100)
 print(sampled)
+
+loaded_mass = []
+loaded_coord = []
+print()
+for a in range(5):
+    print("{} energy".format(a))
+    for k in range(N):
+        j = -N / 2 + 0.5 + k
+        flag = False
+        for i in range(n):
+            if sampled.slice(a, a+1).first.sample['Z{}_{}'.format(i, j)] == 1:
+                print(j, m[i], t[i])
+                loaded_mass.append(m[i])
+                loaded_coord.append(L/N*j)
+                flag = True
+        if not flag:
+            print(j)
+    x = (W_e*x_cg_e + sum([a*b for a,b in zip(loaded_mass, loaded_coord)]))/(W_e + sum(loaded_mass))
+    print(x)
+    print()
